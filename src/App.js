f620aa35d9554a2dba46bedf8a32295a72c0e932
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-01-22 10:27:23 +0200

    restructre files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react'
 function App() {
-  return (
-    <div className='container'>
-      <h2>Advanced Tutorial</h2>
-    </div>
-  )
+	return (
+		<div className="container">
+			<h2>Advanced Tutorial</h2>
+		</div>
+	);
 }
 
-export default App
+export default App;
