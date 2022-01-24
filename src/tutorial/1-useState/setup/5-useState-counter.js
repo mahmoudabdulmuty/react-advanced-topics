11b45430f17e91510eb1cda511c4f5f6f2203971
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-01-24 13:37:42 +0200

    Functional Update Form

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -11,19 +11,37 @@ const UseStateCounter = () => {
 	const handleDecrease = () => {
 		setValue((prevValue) => prevValue - 1);
 	};
+	const increaseLater = () => {
+		setTimeout(
+			() =>
+				setValue((prevValue) => {
+					return prevValue + 1;
+				}),
+			2000
+		);
+	};
 	return (
 		<>
-			<h2>regular counter</h2>
-			<h1>{value}</h1>
-			<button onClick={handleDecrease} className="btn">
-				decrease
-			</button>
-			<button onClick={handleReset} className="btn">
-				reset
-			</button>
-			<button onClick={handleIncrease} className="btn">
-				increase
-			</button>
+			<section>
+				<h2>regular counter</h2>
+				<h1>{value}</h1>
+				<button onClick={handleDecrease} className="btn">
+					decrease
+				</button>
+				<button onClick={handleReset} className="btn">
+					reset
+				</button>
+				<button onClick={handleIncrease} className="btn">
+					increase
+				</button>
+			</section>
+			<section style={{ margin: '4em 0' }}>
+				<h2>Complex regular counter</h2>
+				<h1>{value}</h1>
+				<button onClick={increaseLater} className="btn">
+					increase Later
+				</button>
+			</section>
 		</>
 	);
 };
