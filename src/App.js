78c3f5151d60ee2c0777b25f7d1ab05031beb9a5
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-01-30 20:56:45 +0200

    Show_Hide Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Setup from './tutorial/3-conditional-rendering/setup/2-short-circuit';
+import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide';
 
 function App() {
 	return (
--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+	const [isShown, setIsShown] = useState(false);
+	const [width, setWidth] = useState(window.innerWidth);
+	const handleClick = () => {
+		setIsShown(!isShown);
+	};
+	useEffect(() => {
+		window.addEventListener('resize', () => {
+			setWidth(window.innerWidth);
+		});
+		return () => {
+			window.removeEventListener('resize', () => {
+				setWidth(window.innerWidth);
+			});
+		};
+	}, []);
+	return (
+		<>
+			<button onClick={handleClick} className="btn">
+				Show / Hide Component
+			</button>
+			{isShown && (
+				<>
+					<h1>Window</h1>
+					<h2>Size : {width}</h2>
+				</>
+			)}
+		</>
+	);
 };
 
 export default ShowHide;
