b46818a794fe3a8cfcd892325182dbf8f5edce03
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-02-10 16:12:10 +0200

    Add Item to the List

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,27 +1,63 @@
-// JS
-// const input = document.getElementById('myText');
-// const inputValue = input.value
-// React
-// value, onChange
+import { useState } from 'react';
 
 const ControlledInputs = () => {
+	const [formData, setFormData] = useState({
+		firstName: '',
+		email: '',
+		id: Math.random()
+	});
+	const [people, setPeople] = useState([]);
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log('hello world');
+		if (formData.firstName && formData.email) setPeople([...people, formData]);
+		setFormData({
+			firstName: '',
+			email: '',
+			id: Math.random()
+		});
 	};
+
+	const handleChange = (e) => {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value
+		});
+	};
+
 	return (
 		<article>
 			<form className="form" onSubmit={handleSubmit}>
 				<div className="form-control">
 					<label htmlFor="firstName">Name : </label>
-					<input type="text" id="firstName" name="firstName" />
+					<input
+						type="text"
+						id="firstName"
+						name="firstName"
+						value={formData.firstName}
+						onChange={handleChange}
+					/>
 				</div>
 				<div className="form-control">
 					<label htmlFor="email">Email : </label>
-					<input type="text" id="email" name="email" />
+					<input
+						type="email"
+						id="email"
+						name="email"
+						value={formData.email}
+						onChange={handleChange}
+					/>
 				</div>
 				<button>add person</button>
 			</form>
+			{people.map(({ id, firstName, email }) => {
+				return (
+					<div className="item" key={id}>
+						<h4>{firstName}</h4>
+						<p>{email}</p>
+					</div>
+				);
+			})}
 		</article>
 	);
 };
